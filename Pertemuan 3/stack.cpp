8d63fad65cd53ac60f11db62ef8da2642acbf154
Muzayyannasution <muzayyannasution@gmail.com>
2026-09-25 15:49:21 +0700

    Pertemuan 3

---
+++ Pertemuan 3/stack.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+int main()
+{
+    system("cls");
+    stack<int> tumpukan;
+    int input;
+
+    while(cin >> input)
+    {
+        tumpukan.push(input);
+    }
+
+    do {
+        cout << tumpukan.top() << " ";
+        tumpukan.pop();
+    } while (tumpukan.size() != 0);
+
+    cout << endl;
+    return 0;
+}
